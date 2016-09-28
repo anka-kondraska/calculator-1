6085dc0c93d426569f09fa8dafa785f95870349c
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:38:56 -0700

    added a subtract function

--- arithmetic.py
+++ arithmetic.py
@@ -1,24 +1,34 @@
 def add(num1, num2):
 	""" Returns the sum of two numbers """
+
     return num1 + num2
 
+
 def subtract(num1, num2):
-    pass
+    """ Returns the difference of the second argument from the first argument """
+
+    return num1 - num2
+
 
 def multiply(num1, num2):
     pass
 
+
 def divide(num1, num2):
     pass
+
 
 def square(num1):
     pass
 
+
 def cube(num1):
     pass
 
+
 def power(num1, num2):
     pass
+
 
 def mod(num1, num2):
     pass
